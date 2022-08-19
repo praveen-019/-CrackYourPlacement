29179c7ac14dd75234d06c9f4bad7feb0ed0946e
praveen-019 <72589374+praveen-019@users.noreply.github.com>
2022-08-19 11:53:19 +0530

    Time: 79 ms (45.80%), Space: 13.9 MB (76.74%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        s = [ele for ele in s][::-1]
+        s = s[::-1]
         d = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         count = d[s[0]]
         for i in range(1,len(s)):
