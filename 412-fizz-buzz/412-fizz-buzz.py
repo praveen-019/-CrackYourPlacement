0065adb04bef16fb03874536238cd0d1efa6ac0f
praveen-019 <72589374+praveen-019@users.noreply.github.com>
2022-08-23 19:01:48 +0530

    Time: 66 ms (52.01%), Space: 15.2 MB (17.84%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -2,13 +2,23 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         n += 1
         l = []
+        a = 0
+        b = 0
         for i in range(1,n):
-            if i%3 == 0 and i%5 == 0:
+            if i%3 == 0:
+                a = 1
+            if i%5 == 0:
+                b = 1
+            if a == 1 and b == 1:
                 l.append("FizzBuzz")
-            elif i%3 == 0:
+                a = 0
+                b = 0
+            elif a == 1:
                 l.append("Fizz")
-            elif i%5 == 0:
+                a = 0
+            elif b == 1:
                 l.append("Buzz")
+                b = 0
             else:
                 l.append(str(i))
         return l
